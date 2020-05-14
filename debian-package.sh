91edaf914c4ab8b6403129a5891e80a31c349c2e
Mantas Kriaučiūnas <baltix@gmail.com>
2020-05-14 15:08:37 +0000

    put aw-qt.desktop file to /etc/xdg/autostart and deduplicate libraries
    Decreased .deb package size by ~25MB by hard linking duplicated libraries :)

--- debian-package.sh
+++ debian-package.sh
@@ -4,10 +4,13 @@ set -e
 
 # Following this: http://www.sj-vs.net/creating-a-simple-debian-deb-package-based-on-a-directory-structure/
 
-VERSION_NUM="0.8.0b2"
+VERSION_NUM="0.9.2"
 VERSION="v$VERSION_NUM"
 URL="https://github.com/ActivityWatch/activitywatch/releases/download/${VERSION}/activitywatch-${VERSION}-linux-x86_64.zip"
 PKGDIR="activitywatch_$VERSION_NUM"
+
+#install tools needed for packaging
+sudo apt-get install sed jdupes wget
 
 # Prerun cleanup
 if [ -d "$PKGDIR" ]; then
@@ -18,6 +21,7 @@ fi
 # Create directories
 mkdir -p $PKGDIR/DEBIAN
 mkdir -p $PKGDIR/opt
+mkdir -p $PKGDIR/etc/xdg/autostart
 
 # Move template files into DEBIAN
 cp activitywatch_template/DEBIAN/* $PKGDIR/DEBIAN
@@ -25,9 +29,16 @@ sudo sed -i "s/Version: .*/Version: $VERSION_NUM/g" $PKGDIR/DEBIAN/control
 
 # Get and unzip binary
 wget --continue -O activitywatch-$VERSION-linux.zip $URL
-unzip activitywatch-$VERSION-linux.zip -d $PKGDIR/opt/
+unzip -q activitywatch-$VERSION-linux.zip -d $PKGDIR/opt/
+
+# Hard link duplicated libraries
+jdupes -L -r -S -Xsize-:1K $PKGDIR/opt/
 
 # Set file permissions
 sudo chown -R root:root $PKGDIR
+
+#setup autostart file
+sudo sed -i 's!Exec=aw-qt!Exec=/opt/activitywatch/aw-qt!' $PKGDIR/opt/activitywatch/aw-qt.desktop
+sudo cp $PKGDIR/opt/activitywatch/aw-qt.desktop $PKGDIR/etc/xdg/autostart
 
 dpkg-deb --build $PKGDIR
